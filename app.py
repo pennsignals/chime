562b60dd1134fba4a0919746f6df3d0be0d6d6cd
Hanisha13 <asaf.hanish@uphs.upenn.edu>
2020-03-16 11:06:46 -0400

    updated the regional case count for 2020-03-15

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ montgomery = 826075
 bucks = 628341
 philly = 1581000
 S_default = delaware + chester + montgomery + bucks + philly
-known_infections = 31
+known_infections = 41 #should be updated daily. last upate on 2020-03-15
 
 # Widgets
 initial_infections = st.sidebar.number_input(
@@ -63,7 +63,7 @@ if st.checkbox("Show more info about this tool"):
         "[Discrete-time SIR modeling](https://mathworld.wolfram.com/SIRModel.html) of infections/recovery"
     )
     st.markdown(
-        """The model consists of individuals who are either _Susceptible_ ($S$), _Infected_ ($I$), or _Recovered_ ($R$). 
+        """The model consists of individuals who are either _Susceptible_ ($S$), _Infected_ ($I$), or _Recovered_ ($R$).
 
 The epidemic proceeds via a growth and decline process. This is the core model of infectious disease spread and has been in use in epidemiology for many years."""
     )
@@ -74,7 +74,7 @@ The epidemic proceeds via a growth and decline process. This is the core model o
     st.latex("R_{t+1} = (\\gamma I_t) + R_t")
 
     st.markdown(
-        """To project the expected impact to Penn Medicine, we estimate the terms of the model. 
+        """To project the expected impact to Penn Medicine, we estimate the terms of the model.
 
 To do this, we use a combination of estimates from other locations, informed estimates based on logical reasoning, and best guesses from the American Hospital Association.
 
@@ -82,7 +82,7 @@ To do this, we use a combination of estimates from other locations, informed est
 ### Parameters
 First, we need to express the two parameters $\\beta$ and $\\gamma$ in terms of quantities we can estimate.
 
-- The $\\gamma$ parameter represents 1 over the mean recovery time in days. Since the CDC is recommending 14 days of self-quarantine, we'll use $\\gamma = 1/14$. 
+- The $\\gamma$ parameter represents 1 over the mean recovery time in days. Since the CDC is recommending 14 days of self-quarantine, we'll use $\\gamma = 1/14$.
 - Next, the AHA says to expect a doubling time $T_d$ of 7-10 days. That means an early-phase rate of growth can be computed by using the doubling time formula:
 """)
     st.latex("g = 2^{1/T_d} - 1")
@@ -276,7 +276,7 @@ if st.checkbox("Show Additional Projections"):
 st.subheader("References & Acknowledgements")
 st.markdown(
     """* AHA Webinar, Feb 26, James Lawler, MD, an associate professor University of Nebraska Medical Center, What Healthcare Leaders Need To Know: Preparing for the COVID-19
-* We would like to recognize the valuable assistance in consultation and review of model assumptions by Michael Z. Levy, PhD, Associate Professor of Epidemiology, Department of Biostatistics, Epidemiology and Informatics at the Perelman School of Medicine 
+* We would like to recognize the valuable assistance in consultation and review of model assumptions by Michael Z. Levy, PhD, Associate Professor of Epidemiology, Department of Biostatistics, Epidemiology and Informatics at the Perelman School of Medicine
     """
 )
 st.markdown("© 2020, The Trustees of the University of Pennsylvania")
